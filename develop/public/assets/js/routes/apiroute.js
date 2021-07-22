95f13dedc779441ef4bb28b4ca07cf7ee7782031
RSimer <65471088+RSimer@users.noreply.github.com>
2021-07-22 12:06:44 -0700

    found some sources so found out what to put in the routes (kinda) Ihave no idead what I am doing

---
+++ develop/public/assets/js/routes/apiroute.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+// ask about in tutor session
+router.get("/notes",(req,res)=>{
+
+});
+router.post("/notes",(req,res)=>{
+
+});
+router.delete()
+
+
+
+module.exports = router;
---
+++ develop/public/assets/js/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
