06c2cc1e348601c70be7391ace3716322b04b84e
RSimer <65471088+RSimer@users.noreply.github.com>
2021-07-22 16:01:38 -0700

    post tutor session

---
+++ develop/public/assets/js/routes/htmlRoute.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var router = express.Router()
+const path = require ('path')
+
+router.get("/", (req, res) =>{
+    res.send(path.join(__dirname,"../../../index.html"));
+});
+
+router.get("/notes", (req, res) =>{
+    res.send(path.join(__dirname,"../../../index.html"));
+});
+
+router.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname,"../../../index.html"));
+});
+
+
+
+
+module.exports = router
--- develop/server.js
+++ develop/server.js
@@ -1,17 +1,16 @@
 const express = require('express');
+const apiRoute = require('./public/assets/js/routes/apiRoute');
+const htmlRoute = require('./public/assets/js/routes/htmlRoute');
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
 app.use(express.static('public'));
 
-
-
-
-
-
-
+app.use("/",htmlRoute);
+app.use("/api",apiRoute);
 
 
 
